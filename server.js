0e6d7467b83ed193be0ad9ea8aa6b07698a85ccf
Statikman619 <martinez.michael02@gmail.com>
2021-05-20 17:10:43 -0700

    setting up my server

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// Setting Up My Server
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
