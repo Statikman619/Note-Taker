a3ff72018373a479197c3a138ea47a9eb858f83a
Statikman619 <martinez.michael02@gmail.com>
2021-05-20 17:12:45 -0700

    setting up my POST

--- server.js
+++ server.js
@@ -23,3 +23,19 @@ app.get("/api/notes", function (req, res) {
     res.json(notes);
   });
 });
+
+// API Route | "POST" request
+app.post("/api/notes", function (req, res) {
+  const note = req.body;
+  readFileAsync("./develop/db/db.json", "utf8")
+    .then(function (data) {
+      const notes = [].concat(JSON.parse(data));
+      note.id = notes.length + 1;
+      notes.push(note);
+      return notes;
+    })
+    .then(function (notes) {
+      writeFileAsync("./develop/db/db.json", JSON.stringify(notes));
+      res.json(note);
+    });
+});
